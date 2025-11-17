ea9555e89cb6695e14460ccb49d9d540ecf07066
yazkilito@gmail.com <yazkilito@gmail.com>
2025-11-18 00:15:27 +0100

    create scraping service

---
+++ services/scraping_service.py
@@ -0,0 +1,28 @@
+
+
+from undetected_chromedriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class ScrapingService:
+    def __init__(self):
+        options = ChromeOptions()
+        # options.add_argument('--headless')  # Enlève le headless pour voir la fenêtre
+        options.add_argument('--disable-gpu')
+        options.add_argument('--no-sandbox')
+        self.driver = Chrome(options=options)
+
+    def scraping_service(self):
+        self.driver.get("https://google.com")
+        try:
+                WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable((By.ID, "L2AGLb"))
+                ).click()
+        except Exception as e:
+                print("Bouton 'Tout accepter' non trouvé ou déjà accepté.")
+
+    def close(self):
+        self.driver.quit()
+
+   
