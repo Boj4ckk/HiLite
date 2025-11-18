483b5fa276025c54784d408c40528bc37ef8e549
yazkilito@gmail.com <yazkilito@gmail.com>
2025-11-18 20:30:41 +0100

    add pref to scraper to download clips in data/twitch_clips

--- services/scraping_service.py
+++ services/scraping_service.py
@@ -1,9 +1,12 @@
 
 
+import os
 from undetected_chromedriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+from pathlib import Path
 
 class ScrapingService:
     def __init__(self):
@@ -11,6 +14,12 @@ class ScrapingService:
         # options.add_argument('--headless')  # Enlève le headless pour voir la fenêtre
         options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
+        prefs = {
+            "download.default_directory": str(Path(os.getenv("TWITCH_CLIP_FOLDER_PATH"))),
+            "download.prompt_for_download": False,
+            "directory_upgrade": True
+        }
+        options.add_experimental_option("prefs", prefs)
         self.driver = Chrome(options=options)
 
     def scraping_service(self):
