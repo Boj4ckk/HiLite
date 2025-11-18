9ec4a94b8abb0522abd2f2f94a6f8949ce2a1c9b
yazkilito@gmail.com <yazkilito@gmail.com>
2025-11-18 20:29:32 +0100

    update run to test title gen

--- run.py
+++ run.py
@@ -11,29 +11,35 @@ from buisness.scraping_buisness import ScrapingBuisness
 from services.youtube_service import YoutubeService
 
 
-"""
-load_dotenv()
-twitch_service = TwitchApi(os.getenv("CLIENT_ID"),os.getenv("CLIENT_SECRET"))
 
+load_dotenv()
+
+twitch_service = TwitchApi(os.getenv("TWITCH_CLIENT_ID"),os.getenv("TWITCH_CLIENT_SECRET"))
 twitch_b = TwitchBuisness()
 
 
 
 streamer_id = twitch_service.get_broadcaster_id("Sniper_Biscuit")
-print(streamer_id)
+
 clip_response = twitch_service.get_broadcaster_clips(streamer_id, {"first":1})
 print(clip_response)
+
+game_id = clip_response[0]["game_id"]
+game_info = twitch_service.get_game_info(game_id)
+print(game_info)
+
 clip_url = twitch_b.extract_clips_url(clip_response)
 print(clip_url)
 
+title = twitch_b.generate_short_title(twitch_service,clip_response[0])
 
-
+"""
 s_s = ScrapingService()
 s_b = ScrapingBuisness(s_s)
 s_b.dowload_clip(clip_url[0])
-
 """
+
 load_dotenv()
 
 ys = YoutubeService(os.getenv("CLIENT_SECRET_FILE"),os.getenv("SCOPES"),os.getenv("API_SERVICE_NAME"),os.getenv("API_VERSION"))
-ys.upload_video("20240730_FantasticBreakableLionLitFam-ZzlHJ4he5G7m7cDK_portrait.mp4","ça tue #Short","nouveau #Short",["#Short","#valorant"],"private")
+ys.upload_video("C:/Users/yazki/OneDrive/Bureau/HiLite/HiLite/data/twitch_clips/20240730_FantasticBreakableLionLitFam-ZzlHJ4he5G7m7cDK_portrait.mp4",title,"LE MEILLEUR STREAMER VALO EN LIVE TOUT LES SOIRS",["#Short",twitch_service.get_game_info(clip_response[0]["game_id"]).get("name")],"public")
