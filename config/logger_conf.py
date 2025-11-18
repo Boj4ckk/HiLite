8b10316ac3e7f4e58ac043bf2f2b63760e5b3f2b
yazkilito@gmail.com <yazkilito@gmail.com>
2025-11-18 12:31:05 +0100

    fix handler not found in logger confg

---
+++ config/logger_conf.py
@@ -0,0 +1,30 @@
+
+
+import logging
+import logging.handlers
+
+
+def setup_logger():
+    logger = logging.getLogger("HiLiteLogger")
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        fmt="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+    # File handler with rotation
+    file_handler = logging.handlers.RotatingFileHandler(
+        "app.log", maxBytes=5*1024*1024, backupCount=3
+    )
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+
+    return logger
