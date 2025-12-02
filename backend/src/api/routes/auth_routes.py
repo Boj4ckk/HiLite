fb5a8a59ae84bce6a0f7f9528587a216f8308e80
yazkilito@gmail.com <yazkilito@gmail.com>
2025-12-02 22:01:57 +0100

    update auth route

--- backend/src/api/routes/auth_routes.py
+++ backend/src/api/routes/auth_routes.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Body, Depends
 
-from api.dependencies import get_current_user
+from api.dependencies import get_current_user, get_user_repository
+from models.twitch_token_model import TwitchTokensRequest
 from config.logger_conf import setup_logger
 from models.user_model import User
+from repositories.user_repository import UserRepository
 
 logger = setup_logger()
 
@@ -13,3 +15,15 @@ router = APIRouter(prefix="/auth")
 @router.get("/me")
 def protected_route(current_user: User = Depends(get_current_user)):
     return {"user": current_user}
+
+@router.post("/twitch/tokens")
+async def save_twitch_tokens(
+    tokens : TwitchTokensRequest,
+    user_respository: UserRepository = Depends(get_user_repository)
+    
+    ):
+    access_token = tokens.twitch_access_token
+    refresh_token = tokens.twitch_refresh_token
+
+    user_respository.insert_twitch_tokens(access_token,refresh_token)
+    
