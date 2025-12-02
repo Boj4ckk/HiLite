1dd23a4c2df4f6b6a13463dd6d78f021ef5518e4
yazkilito@gmail.com <yazkilito@gmail.com>
2025-12-02 17:38:31 +0100

    run:app

--- backend/src/run.py
+++ backend/src/run.py
@@ -9,4 +9,4 @@ setup_cors(app)
 app.include_router(auth_router, tags=["auth"])
 
 if __name__ == "__main__":
-    uvicorn.run("src.run:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
