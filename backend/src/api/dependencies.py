47f95d0f52179c6c434b3d23dc3574a9bca9cf88
yazkilito@gmail.com <yazkilito@gmail.com>
2025-12-02 17:38:23 +0100

    add get_token

--- backend/src/api/dependencies.py
+++ backend/src/api/dependencies.py
@@ -31,15 +31,24 @@ def get_user_business(
     user_buisness = UserBusiness(supabase, user_repository)
     return user_buisness
 
-
-async def get_current_user(
-    authorization: str = Header(None),
-    user_business: UserBusiness = Depends(get_user_business),
+async def get_user_token(
+        authorization: str = Header(None)
 ):
     if authorization is None:
         raise HTTPException(status_code=401, detail="Missing auth header")
+    token = authorization.replace("Bearer", "").strip()
 
-    token = authorization.replace("Bearer ", "").strip()
+    return {"token" : token}
+
+
+async def get_current_user(
+    user_token: dict = Depends(get_user_token),
+    user_business: UserBusiness = Depends(get_user_business),
+):
+    token = user_token.get("token")
     db_user = user_business.sync_user(token)
 
-    return {"user": db_user}
+    return {
+        "user": db_user
+    }
+
